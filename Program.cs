e83dbc19793d81d608d10444ce368e1fb73845e4
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2026-01-03 15:41:25 -0500

    Cambie la conexion de la base de datos del archivo .env a app.json

--- Program.cs
+++ Program.cs
@@ -1,35 +1,43 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using technical_test_sigma.Application.Interfaces.Customer;
 using technical_test_sigma.Application.Services.CustomerService;
 using technical_test_sigma.Infrastructure.Data;
 using technical_test_sigma.Infrastructure.Repositories.Customers;
 using DotNetEnv;
 
-// Carga las variables desde el archivo .env
-Env.Load();
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 🔹 Cargar variables del .env
+Env.Load();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDb")));
-
-//Inyeccion de dependencias
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-builder.Services.AddHttpClient<ICrmService, HubSpotCrmService>();
-
+// 🔹 Leer variables de entorno
 var hubspotBaseUrl = Environment.GetEnvironmentVariable("HUBSPOT_BASE_URL");
 var hubspotToken = Environment.GetEnvironmentVariable("HUBSPOT_ACCESS_TOKEN");
 
+// 🔹 DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionDb"))
+);
+
+// 🔹 Inyección de dependencias
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+// 🔹 HttpClient para HubSpot
+builder.Services.AddHttpClient<ICrmService, HubSpotCrmService>(client =>
+{
+    client.BaseAddress = new Uri(hubspotBaseUrl);
+    client.DefaultRequestHeaders.Authorization =
+        new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", hubspotToken);
+});
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -37,9 +45,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
