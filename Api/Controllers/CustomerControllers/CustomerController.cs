2a0060669256dc71d70e3ee0d29b0f15e4d54690
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2026-01-02 22:21:00 -0500

    Controlador terminado

---
+++ Api/Controllers/CustomerControllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using technical_test_sigma.Application.Interfaces.Customer;
+using technical_test_sigma.DTO.CustomerDto;
+
+namespace technical_test_sigma.Api.Controllers.CustomerControllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        /// <summary>
+        /// Registro completo del cliente (datos + dirección + pago)
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] CustomerCreateDto dto)
+        {
+            var customerId = await _customerService.CreateCustomerAsync(dto);
+            return Ok(new { CustomerId = customerId });
+        }
+
+        /// <summary>
+        /// Consulta del estado del cliente
+        /// </summary>
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetCustomerStatus(Guid id)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            return Ok(customer);
+        }
+
+        /// <summary>
+        /// Activar o desactivar un cliente
+        /// </summary>
+        [HttpPut("{id:guid}/status")]
+        public async Task<IActionResult> ChangeCustomerStatus(
+            Guid id,
+            [FromBody] CustomerStatusDto dto)
+        {
+            await _customerService.ChangeStatusAsync(id, dto.Active);
+            return NoContent();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using technical_test_sigma.Application.Interfaces.Customer;
+using technical_test_sigma.Application.Services.CustomerService;
 using technical_test_sigma.Infrastructure.Data;
+using technical_test_sigma.Infrastructure.Repositories.Customers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDb")));
+
+//Inyeccion de dependencias
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
